23304cfc9adce90e0f2a96940d1499f769775c50
Samuel Hübner <samu.huebner@gmail.com>
2020-12-21 19:54:54 +0100

    Updates Database methods

--- app/src/main/java/de/samuelhuebner/shopit/database/Database.java
+++ app/src/main/java/de/samuelhuebner/shopit/database/Database.java
@@ -1,5 +1,61 @@
 package de.samuelhuebner.shopit.database;
 
-public class Database {
+import android.content.Context;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Database {
+    private final DatabaseHelper dbHelper;
+
+    private static final HashMap<String, ShoppingList> map = new HashMap<>();;
+    private static ArrayList<String> names = new ArrayList<>();
+
+    static {
+        ShoppingList tmp = new ShoppingList("Test1");
+
+        map.put(tmp.getUuid(), tmp);
+        names.add(tmp.getName());
+    }
+
+    public Database(Context context) {
+        dbHelper = new DatabaseHelper(context);
+    }
+
+    /**
+     * Creates a new shopping list and puts it into the hash map
+     * @param name      The name of the new shopping list
+     * @return          The new ShoppingList object
+     */
+    public ShoppingList createShoppingList(String name) {
+        ShoppingList newList = new ShoppingList(name);
+        map.put(newList.getUuid(), newList);
+
+        names.add(newList.getName());
+        return newList;
+    }
+
+    /**
+     * Returns a ShoppingLists array of the current shopping lists
+     * @return
+     */
+    public ShoppingList[] getShoppingLists() {
+        String[] keys = (String[]) map.keySet().toArray();
+
+        ShoppingList[] shoppingLists = new ShoppingList[keys.length];
+
+        for (int i = 0; i < shoppingLists.length; i++) {
+            shoppingLists[i] = map.get(keys[i]);
+        }
+
+        return shoppingLists;
+    }
+
+    public ArrayList<String> getShoppingListsNames() {
+        for (String name : names) {
+
+        }
+        return names;
+    }
 }
--- app/src/main/java/de/samuelhuebner/shopit/database/DatabaseHelper.java
+++ app/src/main/java/de/samuelhuebner/shopit/database/DatabaseHelper.java
@@ -1,4 +1,9 @@
 package de.samuelhuebner.shopit.database;
 
+import android.content.Context;
+
 public class DatabaseHelper {
+    public DatabaseHelper(Context context) {
+
+    }
 }
