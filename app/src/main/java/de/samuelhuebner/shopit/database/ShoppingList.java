e77073eaf8b8a52f418aafb29b16ac9f4ac204de
Samuel Hübner <samu.huebner@gmail.com>
2020-12-21 19:56:03 +0100

    Adds UUID field to ShoppingList

--- app/src/main/java/de/samuelhuebner/shopit/database/ShoppingList.java
+++ app/src/main/java/de/samuelhuebner/shopit/database/ShoppingList.java
@@ -1,8 +1,12 @@
 package de.samuelhuebner.shopit.database;
 
+import android.util.Log;
+
 import java.util.ArrayList;
+import java.util.UUID;
 
 public class ShoppingList {
+    private String uuid;
     private String name;
     private ArrayList<ListPosition> positions;
     private boolean isComplete;
@@ -11,6 +15,8 @@ public class ShoppingList {
         this.name = name;
         this.positions = new ArrayList<>();
         this.isComplete = false;
+
+        this.uuid = UUID.randomUUID().toString().replace("-", "");
     }
 
     public void addPosition(ListPosition newPos) {
@@ -21,4 +27,8 @@ public class ShoppingList {
     public ListPosition[] getPositions() {
         return (ListPosition[]) positions.toArray();
     }
+
+    public String getUuid() { return uuid; }
+
+    public String getName() { return name; }
 }
